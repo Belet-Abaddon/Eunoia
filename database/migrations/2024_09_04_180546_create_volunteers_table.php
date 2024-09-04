2c5f020ad27da66df6d15ca1c99d172dfbf399a1
Meowveloper <zwekhantaung22.tgi@gmail.com>
2024-09-05 01:06:36 +0700

    tables and model fix

--- database/migrations/2024_09_04_180546_create_volunteers_table.php
+++ database/migrations/2024_09_04_180546_create_volunteers_table.php
@@ -11,15 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('volunteer', function (Blueprint $table) {
+        Schema::create('volunteers', function (Blueprint $table) {
             $table->id();
-            $table->string('v_name');
-            $table->string('email');
-            $table->string('password');
-            $table->string('address');
-            $table->string('gender');
-            $table->string('degree');
-            $table->integer('experience');
+            $table->timestamps();
         });
     }
 
@@ -28,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('volunteer');
+        Schema::dropIfExists('volunteers');
     }
 };
