f954350782fb385563ec37581678a94c9b3ca029
Meowveloper <zwekhantaung22.tgi@gmail.com>
2024-09-10 02:09:52 +0700

    ui setup

---
+++ database/factories/AnswerFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Answer>
+ */
+class AnswerFactory extends Factory
+{
+    protected $model = App\Models\Answer::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'answer' => fake()->name(),
+            'percentage' => random_int(0, 100),
+            'user_id' => App\Models\User::factory(),
+            
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +15,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'age' => 20,
+            'gender' => 'female',
+            'country' => 'Myanmar',
+            'degree' => 'Psychologist',
+            'experience' => 7,
+            'password' => Hash::make(env('ADMIN_PASSWORD')),
         ]);
     }
 }
